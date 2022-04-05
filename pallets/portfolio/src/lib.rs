274804fcf53481adc13e278bfe69f837ac3451d1
yubo <anhuitianyu@163.com>
2022-04-05 20:50:28 +0800

    other

--- pallets/portfolio/src/lib.rs
+++ pallets/portfolio/src/lib.rs
@@ -1,4 +1,5 @@
 #![cfg_attr(not(feature = "std"), no_std)]
+#![recursion_limit = "256"]
 
 pub use pallet::*;
 
@@ -15,31 +16,41 @@ mod benchmarking;
 pub mod pallet {
 	use frame_support::{
 		pallet_prelude::*,
-		traits::{Currency,ExistenceRequirement, tokens::fungibles::{Create, Transfer} },
-		PalletId, Twox128,
+		traits::{
+			fungible::{self, Transfer as NativeTransfer},
+			tokens::fungibles::{self, Create, Mutate, Transfer},
+			Currency,
+		},
+		PalletId,
 	};
-	use frame_system::Origin;
-
 
 	use frame_system::pallet_prelude::*;
-	use sp_runtime::{
-		traits::{
-			AccountIdConversion, AtLeast32BitUnsigned, Bounded, CheckedAdd, CheckedSub, Saturating,
-			StaticLookup, Zero,
-		},
-		ArithmeticError, TokenError,
-	};
+	use sp_runtime::traits::{AccountIdConversion, One, StaticLookup};
 	use sp_std::vec::Vec;
 
+	type AssetIdOf<T> = <<T as Config>::Currency as fungibles::Inspect<
+		<T as frame_system::Config>::AccountId,
+	>>::AssetId;
+	type BalanceOf<T> = <<T as Config>::Currency as fungibles::Inspect<
+		<T as frame_system::Config>::AccountId,
+	>>::Balance;
+	type NativeBalanceOf<T> = <<T as Config>::NativeCurrency as fungible::Inspect<
+		<T as frame_system::Config>::AccountId,
+	>>::Balance;
 
-	/// Configure the pallet by specifying the parameters and types on which it depends.
 	#[pallet::config]
-	pub trait Config: frame_system::Config + pallet_assets::Config {
+	pub trait Config: frame_system::Config {
 		/// Because this pallet emits events, it depends on the runtime's definition of an event.
 		type Event: From<Event<Self>> + IsType<<Self as frame_system::Config>::Event>;
 
 		#[pallet::constant]
 		type PalletId: Get<PalletId>;
+
+		type Currency: fungibles::Create<Self::AccountId>
+			+ fungibles::Mutate<Self::AccountId>
+			+ fungibles::Transfer<Self::AccountId>;
+
+		type NativeCurrency: NativeTransfer<Self::AccountId>;
 	}
 
 	#[pallet::pallet]
@@ -50,18 +61,16 @@ pub mod pallet {
 	#[pallet::storage]
 	#[pallet::getter(fn components)]
 	pub type Components<T: Config> =
-		StorageMap<_, Twox64Concat, T::AssetId, Vec<T::AssetId>, ValueQuery>;
+		StorageMap<_, Twox64Concat, AssetIdOf<T>, Vec<AssetIdOf<T>>, ValueQuery>;
 
 	#[pallet::storage]
 	#[pallet::getter(fn rates)]
-	pub type Rates<T: Config> = StorageMap<_, Twox64Concat, T::AssetId, Vec<u32>, ValueQuery>;
+	pub type Rates<T: Config> = StorageMap<_, Twox64Concat, AssetIdOf<T>, Vec<u32>, ValueQuery>;
 
 	#[pallet::storage]
 	#[pallet::getter(fn owners)]
-	pub type Owners<T:Config> = StorageMap<_,Twox64Concat, T::AssetId, T::AccountId>;
+	pub type Owners<T: Config> = StorageMap<_, Twox64Concat, AssetIdOf<T>, T::AccountId>;
 
-	// Pallets use events to inform users when important changes are made.
-	// https://docs.substrate.io/v3/runtime/events-and-errors
 	#[pallet::event]
 	#[pallet::generate_deposit(pub(super) fn deposit_event)]
 	pub enum Event<T: Config> {
@@ -84,35 +93,28 @@ pub mod pallet {
 		}
 	}
 
-	//impl<T: Config<I>,I:'static = ()> Pallet<T> {
 	#[pallet::call]
 	impl<T: Config> Pallet<T> {
 		#[pallet::weight(10_000)]
 		pub fn create_portofio(
 			origin: OriginFor<T>,
-			#[pallet::compact] id: T::AssetId,
-			admin: <T::Lookup as StaticLookup>::Source,
-			components: Vec<T::AssetId>,
+			id: AssetIdOf<T>,
+			components: Vec<AssetIdOf<T>>,
 			exchange_rates: Vec<u32>,
-			mint_amount: T::Balance,
+			mint_amount: BalanceOf<T>,
 		) -> DispatchResult {
 			ensure!(components.len() == exchange_rates.len(), Error::<T>::NotEquel);
 			for cid in &components {
 				//ensure!(pallet_assets::Pallet::Asset::<T>::contains_key(cid),
 				// Error::<T>::NotExistId);
 			}
-			pallet_assets::Pallet::<T>::force_create(origin.clone(), id, admin, true,T::Balance::from(1u32))?;
-			pallet_assets::Pallet::<T>::mint(
-				origin.clone(),
-				id,
-				<T::Lookup as StaticLookup>::unlookup(Self::account_id()),
-				mint_amount,
-			)?;
+			let admin = ensure_signed(origin)?;
+			T::Currency::create(id, admin.clone(), true, BalanceOf::<T>::one())?;
+			T::Currency::mint_into(id, &Self::account_id(), mint_amount)?;
 
-			let owner = ensure_signed(origin)?;
 			Components::<T>::insert(id, components);
 			Rates::<T>::insert(id, exchange_rates);
-			Owners::<T>::insert(id,owner);
+			Owners::<T>::insert(id, admin);
 
 			Ok(())
 		}
@@ -120,56 +122,40 @@ pub mod pallet {
 		#[pallet::weight(10_000)]
 		pub fn create_contract(
 			origin: OriginFor<T>,
-			#[pallet::compact] id: T::AssetId,
-			mint_amount: T::Balance,
+			id: AssetIdOf<T>,
+			mint_amount: BalanceOf<T>,
 		) -> DispatchResult {
 			let owner = ensure_signed(origin.clone())?;
-			pallet_assets::Pallet::<T>::force_create(origin.clone(), id,<T::Lookup as StaticLookup>::unlookup(owner), true,T::Balance::from(1u32))?;
-			pallet_assets::Pallet::<T>::mint(
-				origin.clone(),
-				id,
-				<T::Lookup as StaticLookup>::unlookup(Self::account_id()),
-				mint_amount,
-			)?;
+			T::Currency::create(id, owner, true, BalanceOf::<T>::one())?;
+			T::Currency::mint_into(id, &Self::account_id(), mint_amount)?;
 
 			Ok(())
 		}
 
 		#[pallet::weight(10_000)]
-		pub fn buy(
-			origin: OriginFor<T>,
-			#[pallet::compact] id: T::AssetId,
-			amount:u32,
-		) -> DispatchResult {
+		pub fn buy(origin: OriginFor<T>, id: AssetIdOf<T>, amount: u32) -> DispatchResult {
 			let who = ensure_signed(origin)?;
 			ensure!(Components::<T>::contains_key(id), Error::<T>::NotHasAsset);
 			let owner = Owners::<T>::get(id).ok_or(Error::<T>::NotEquel)?;
-			T::Currency::transfer(&who,&owner, amount.into(),ExistenceRequirement::KeepAlive)?;
+			T::NativeCurrency::transfer(&who, &owner, amount.into(), true)?;
 
-			pallet_assets::Pallet::<T>::transfer(Origin::<T>::Signed(Self::account_id()).into(),
-				id,<T::Lookup as StaticLookup>::unlookup(who),amount.into())?;
+			T::Currency::transfer(id, &Self::account_id(), &who, amount.into(), true)?;
 			Ok(())
 		}
 
 		#[pallet::weight(10_000)]
-		pub fn sell(
-			origin: OriginFor<T>,
-			#[pallet::compact] id: T::AssetId,
-			amount: u32,
-		) -> DispatchResult {
+		pub fn sell(origin: OriginFor<T>, id: AssetIdOf<T>, amount: u32) -> DispatchResult {
 			let who = ensure_signed(origin.clone())?;
 
 			ensure!(Components::<T>::contains_key(id), Error::<T>::NotHasAsset);
-			pallet_assets::Pallet::<T>::transfer(origin.clone(),
-				id,<T::Lookup as StaticLookup>::unlookup(Self::account_id()),amount.into())?;
+			T::Currency::transfer(id, &who, &Self::account_id(), amount.into(), true)?;
 
 			let ids = Components::<T>::get(id);
 			let rate = Rates::<T>::get(id);
 			for i in 0..ids.len() {
 				let id = ids[i];
 				let rate = rate[i];
-				pallet_assets::Pallet::<T>::transfer(Origin::<T>::Signed(Self::account_id()).into(),
-				id,<T::Lookup as StaticLookup>::unlookup(who.clone()),(amount*rate).into())?;
+				T::Currency::transfer(id, &Self::account_id(), &who, (amount * rate).into(), true)?;
 			}
 			Ok(())
 		}
